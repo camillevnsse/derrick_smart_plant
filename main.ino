85205317ab28811cb4e445fa7223eec7f647e893
LuckyLuke015 <102171576+LuckyLuke015@users.noreply.github.com>
2022-11-01 17:40:22 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -6,13 +6,13 @@ DHT dht(2, DHT11);
 
 // sensor setup
 void setup() {
-	dht.begin();
-  Serial.begin(9600) // exchange rate of data : 9600 bits/s 
+	dht.begin() ;
+	Serial.begin(9600) ; // exchange rate of data : 9600 bits/s 
 }
 
 // read and display info given by dht
 void loop() {
-	 float h = dht.readHumidity() ;
+	float h = dht.readHumidity() ;
 	float t = dht.readTemperature() ;
 
 	Serial.print("Humidity :") ;
