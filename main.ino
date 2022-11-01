2aed1acd6de923017cc98a1eda199470a1e155fe
camille.vnsse <38594439+camillevnsse@users.noreply.github.com>
2022-11-01 17:55:51 +0100

    added LCD display

--- main.ino
+++ main.ino
@@ -1,22 +1,41 @@
-// DHT library import 
+// library imports (for the DHT11 sensor and the LCD display) 
 #include "DHT.h"
+#include "LiquidCrystal_I2C.h"
+#include "Wire.h"
 
-// define on which pin the information given by the dht sensor will be read
+// components setup
 DHT dht(2, DHT11);
+LiquidCrystal_I2C LCD(0x27,16,2);
 
 // sensor setup
 void setup() {
+	LCD.init();
+   	LCD.backlight();
 	dht.begin() ;
-	Serial.begin(9600) ; // exchange rate of data : 9600 bits/s 
+	// Serial.begin(9600) ; // exchange rate of data : 9600 bits/s 
 }
 
-// read and display info given by dht
 void loop() {
+	// read data from the DHT11
 	float h = dht.readHumidity() ;
 	float t = dht.readTemperature() ;
+	
+	// humidity display
+	LCD.setCursor(0,0); // top-left
+	LCD.print("Humidity: ");
+	LCD.print(h);
+	
+	// temperature display
+	LCD.setCursor(0,1); // bottom-left
+	LCD.print("Temperature: ");
+	LCD.print(t);
+	
+	// refresh
+	delay(1000);
+	LCD.clear();
 
-	Serial.print("Humidity :") ;
-	Serial.printIn(h) ;
-	Serial.print("Temperature :") ;
-	Serial.printIn(t) ;
+	// Serial.print("Humidity :") ;
+	// Serial.printIn(h) ;
+	// Serial.print("Temperature :") ;
+	// Serial.printIn(t) ;
 }
