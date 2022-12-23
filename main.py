4556449e333967f944eb11a2df456979aafca766
camille.vnsse <38594439+camillevnsse@users.noreply.github.com>
2022-12-23 16:41:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, render_template, request
 from notification import NotificationApi
+from watering import WateringApi
 import db_test
 
 app = Flask(__name__)
@@ -57,6 +58,12 @@ def get_weather():
 @app.route("/notification")
 def test_notif():
     NotificationApi.test_notification()
+    return json.dumps({"status": 200, "notification_status": "correct"})
+
+
+@app.route("/watering")
+def watering():
+    WateringApi.water()
     return json.dumps({"status": 200, "notification_status": "correct"})
 
 
