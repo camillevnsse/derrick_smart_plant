a0248fd47743aed6d46aafde6f1db5efc05e5312
camille.vnsse <38594439+camillevnsse@users.noreply.github.com>
2023-02-24 23:09:16 +0100

    Update test_client.py

--- test_client.py
+++ test_client.py
@@ -1,55 +1,21 @@
-# sensors
-
-import requests
 import json
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 from random import random
-from datetime import datetime
 from time import sleep
 from main.db_test2 import add_data, reset_db
-
-app = Flask(__name__)
-
-
-def get_current_datetime():
-    now = datetime.now()
-    return now.strftime("%m/%d/%Y %H:%M:%S")
-        
-
-@app.route('/')
-def index():
-    return 'Rien à afficher ici'
-
-    
-@app.route("/watering", methods=["POST"])
-def watering():
-    freq = request.json["freq"]
-    if freq:
-        return freq
-    else:
-        return "1" # fréquence d'arrosage par défaut : tous les jours
-
-
-@app.route("/data_freq", methods=["POST"])
-def data_freq():
-    data_freq = request.json["data_freq"]
-    if data_freq:
-        return data_freq
-    else:
-        return "1" # fréquence d'envoi des données par défaut : toutes les secondes
+from main.routes import freq as data_freq
+from main.routes import water_freq as water_freq
 
 
 while True:
-        # valeurs temporairement aléatoires : à fortiori ce sera les valeurs envoyées par les sensors
-        add_data("humidity", round(random() * 100, 1), "test hum")
-        add_data("temperature", round(random() * 100, 1), "test temp")
-        add_data("water_lvl", round(random() * 100, 1), "test wat")
-        sleep(3)
+    # valeurs temporairement aléatoires : à fortiori ce sera les valeurs envoyées par les sensors
+    add_data("humidity", round(random() * 100, 1), "test hum")
+    add_data("temperature", round(random() * 100, 1), "test temp")
+    add_data("water_lvl", round(random() * 100, 1), "test wat")
 
+    sleep(3)
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
-    
-
-
+    '''if data_freq:
+        sleep(data_freq)
+    else:
+        sleep(3)'''
