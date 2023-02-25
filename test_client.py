1604a40e19868c456c6be4d033b454fc2cb0777d
camille.vnsse <38594439+camillevnsse@users.noreply.github.com>
2023-02-25 15:53:27 +0100

    Add files via upload

--- test_client.py
+++ test_client.py
@@ -2,20 +2,25 @@ import json
 from flask import Flask, render_template, request
 from random import random
 from time import sleep
-from main.db_test2 import add_data, reset_db
-from main.routes import freq as data_freq
-from main.routes import water_freq as water_freq
-
+# from main.db_test2 import add_data, reset_db
+# from main.routes import freq as data_freq
+# from main.routes import water_freq as water_freq
+import requests
 
 while True:
+    print('ok')
     # valeurs temporairement aléatoires : à fortiori ce sera les valeurs envoyées par les sensors
-    add_data("humidity", round(random() * 100, 1), "test hum")
-    add_data("temperature", round(random() * 100, 1), "test temp")
-    add_data("water_lvl", round(random() * 100, 1), "test wat")
+
+    # TODO: faire requête post vers /add_data pour envoyer données des sensors
+    hum_value = round(random() * 100, 1)
+    temp_value = round(random() * 100, 1)
+    water_lvl_value = round(random() * 100, 1)
+    url = "http://127.0.0.1:5000/store_data"
+
+    data = requests.post(url, json={"humidity": hum_value, "temperature": temp_value, "water_lvl": water_lvl_value})
+    '''add_data("humidity", round(random() * 100, 1), "hum")
+    add_data("temperature", round(random() * 100, 1), "temp")
+    add_data("water_lvl", round(random() * 100, 1), "wat")'''
 
     sleep(3)
-
-    '''if data_freq:
-        sleep(data_freq)
-    else:
-        sleep(3)'''
+    print('ok2')
