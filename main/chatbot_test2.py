6abaa14ad80531c650234dfec71d0f522585485b
Luke <102171576+LuckyLuke015@users.noreply.github.com>
2023-03-05 11:55:02 +0100

    Add files via upload

--- main/chatbot_test2.py
+++ main/chatbot_test2.py
@@ -2,8 +2,12 @@ from nltk.chat.util import Chat, reflections
 
 pairs = [
     [
-        r"(mon nom est|je m'appelle) (.*)",
-        ["Bonjour %2, comment allez-vous aujourd'hui?", "Salut %2, ravi de te rencontrer."]
+        r"Derrick",
+        ["C'est bien moi ! Quel est votre nom ?", "A votre service ! Comment puis-je vous aider ?"]
+    ],
+    [
+        r"(.*) (mon nom est|je m'appelle) (.*)",
+        ["Bonjour %2, comment allez-vous aujourd'hui?", "Salut %3, ravi de te rencontrer."]
     ],
     [
         r"comment tu t'appelles|quel est ton nom|comment t'appelles-tu|qui es-tu|qu'est-ce que tu es",
@@ -12,6 +16,10 @@ pairs = [
     [
         r"à quoi sers-tu",
         ["Je suis un chatbot, et je suis là pour vous aider. Posez-moi une question, je ferai de mon mieux pour répondre !"]
+    ],
+    [
+        r"(.*) (bien|super|ça va) (.*) et toi ?",
+        ["Je vais bien, merci !"]
     ],
     [
         r"ça va bien|je vais bien|ça va super",
@@ -27,7 +35,7 @@ pairs = [
     ],
     [
         r"salut|hey|hello|bonjour|coucou",
-        ["Salut!", "Bonjour!", "Bonjour, comment allez-vous ?", "Bonjour, quel est votre nom ?"]
+        ["Salut!", "Bonjour!", "Bonjour, comment allez-vous ?", "Bonjour, quel est votre nom ?", "Bonjour ! En quoi puis-je vous aider ?", "Salut, je m'appelle Derrick !"]
     ],
     [
         r"(.*) âge?",
@@ -48,9 +56,8 @@ pairs = [
         ['A Buc, en Ile de France!', "Au Lycée Franco-Allemand de Buc, un lycée d'exception"]
     ],
     [
-        r"(.*)météo à (.*)?",
-        ["La météo à %2 est incroyable comme toujours", "Beaucoup trop chaud à %2, je vais fondre", "Beaucoup trop froid à %2, mes circuits vont geler",
-         "Jamais entendu parler de %2"]
+        r"(.*)(météo|temps fait-il)(.*)?",
+        ["La météo %3 est incroyable comme toujours", "Beaucoup trop chaud %3, je vais fondre", "Beaucoup trop froid %3, mes circuits vont geler",]
     ],
 
     [
@@ -64,16 +71,16 @@ pairs = [
     ],
 
     [
-        r"quand arroser (.*) plante en pot",
+        r"quand arroser (.*) (plante en pot|plante d'intéreieur|plante verte)",
         ["La fréquence d’arrosage dépend de la saison (on arrose évidemment plus en été qu’en hiver), de la météo, mais aussi de la taille du pot et surtout de la plante à arroser. Tous les végétaux n’ont pas les mêmes besoins, il faut donc adapter les arrosages à ces derniers. Certaines plantes nécessitent un substrat constamment frais sous peine de voir les feuilles prendre une mine plombée et s’affaisser. D’autres, comme les plantes méditerranéennes et la majorité des aromatiques, supportent mal les excès d’eau et apprécient un bon drainage du terreau. On attend que le terreau sèche un peu en surface entre deux arrosages."]
     ],
 
     [
-        r"comment (arroser|entretenir)(.*)cactus",
+        r"(.*)(comment|quand)(.*)(arroser|arrose|entretenir|entretient)(.*)cactus",
         ["Un cactus n’a pas besoin d’être arrosé régulièrement, même si lorsqu’il fait chaud, les apports en eau sont plus fréquents. Observez le substrat pour savoir si oui ou non votre cactus a besoin d’eau. Si la terre est sèche, place à l’arrosoir ! N’hésitez pas à hydrater abondamment votre plante, mais prenez garde à ne pas laisser l’eau stagner dans la soucoupe. Cela pourrait faire mourir prématurément votre cactus. Ne mouillez pas le feuillage au moment de l’arrosage, mais vaporisez de temps en temps de l’eau sur le corps du cactus pour l’hydrater au maximum."]
     ],
     [
-        r"(comment|quand) (arroser|entretenir)(.*)plante verte",
+        r"(.*)(comment|quand)(.*)(arroser|arrose|entretenir|entretient)(.*)(plante verte|plante en pot|plante d'intéreieur)",
         ["Une plante verte a besoin d'être arrosée régulièrement, mais pas trop généreusement. Arrosez donc votre plante doucement avec de l'eau à température ambiante au moins une fois par jour"]
     ],
     [
