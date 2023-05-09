69fca9c38107ccabcaf4eb3a3f47d79ea1064eb5
camille.vnsse <38594439+camillevnsse@users.noreply.github.com>
2023-05-09 10:20:25 +0200

    Update watering.py

--- watering.py
+++ watering.py
@@ -1,11 +1,16 @@
-# envoie une notification au téléphone portable via Pushbullet lorsque la plante est arrosée
-
-import json
-
+import requests
 from pushbullet import Pushbullet
 
 
 class WateringApi:
     def water():
+        print('ok')
+        try:
+            requests.post("http://192.168.0.39/water")
+        except Exception as err:
+            print(err)
+
         pb = Pushbullet("o.gFohQYe13yA2DzNP2J199J7qZQF2R34S")
         push = pb.push_note("Derrick", "Arrosage de votre plante en cours...")
+
+        print('plante arrosée !')
